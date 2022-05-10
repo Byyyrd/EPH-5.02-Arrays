c3ab8e74840770ea16507c48e6bb26b3cabd0997
aos <aos@kkg-do.eu>
2022-05-10 09:57:32 +0200

    Initial-Commit: Übungsprojekt zum Umgang mit Arrays in der EPH.

---
+++ src/MainProgram.java
@@ -0,0 +1,140 @@
+class MainProgramm {
+
+    //Attribute
+    private static boolean withNegatives;
+    private static int maxAbsolute;
+
+    //Deklaration
+    private static int[] numbers;
+
+    public static void main(String[] args){
+        withNegatives = false;
+        maxAbsolute = 100;
+        numbers = new int[10];
+        fillArray(numbers, withNegatives, maxAbsolute);
+
+        showArray(numbers);
+
+        //Hier sehen Sie Beispiele, wie man Methoden mit Rückgabewert aufrufen und dank der Systemkonsole testen kann.
+        //Bevor die Methoden umgesetzt sind, werden sie nur 0 zurückgeben.
+        System.out.println("Summe aller Zahlen im Array: " + sumUp(numbers));
+        System.out.println("In diesem Array gibt es " + countNegatives(numbers) + " negative Zahlen.");
+        System.out.println("Summe der negativen Zahlen: " + sumUpNegatives(numbers));
+    }
+
+    private static void fillArray(int[] array, boolean wN, int maxA){
+        if(wN){
+            for(int i = 0; i < array.length; i++){
+                int power = (int)(Math.random()*2);
+                array[i] = (int)(Math.pow(-1,power)*(Math.random()*(maxA+1)));
+            }
+        }else{
+            for(int i = 0; i < numbers.length; i++){
+                array[i] = (int)(Math.random()*(maxA+1));
+            }
+        }
+    }
+
+    /**
+     * Diese Methode stellt alle Werte des übergeben Arrays samt dazugehörigem Index in der Systemkonsole dar.
+     * @param array
+     */
+    private static void showArray(int[] array){
+        if(array != null){
+            for(int i = 0; i < array.length; i++){
+                System.out.println("Index: "+i+" --- Zahl: "+array[i]);
+            }
+        }
+    }
+
+
+    /**
+     * 1. Alle Werte aufaddieren
+     * Schreiben Sie eine Methode sumUp, die als Parameter ein Array des Typs int übergeben bekommt. Die Methode sumUp soll die enthaltenen Zahlen addieren und das Ergebnis zurückliefern.
+     * @param array
+     * @return
+     */
+    private static int sumUp(int[] array){
+        int result = 0;
+        //Hier muss Quellcode ergänzt werden.
+        return result;
+    }
+
+
+    /**
+     * 2. Negative Werte zählen - hierzu bitte in Zeile 11 withNegatives = true setzen.
+     * Schreiben Sie eine Methode countNegatives, die als Parameter ein Array des Typs int übergeben bekommt. Die Methode countNegatives soll die negativen Werte in diesem Array zählen und das Ergebnis als zurückgeben.
+     * @param array
+     * @return
+     */
+    private static int countNegatives(int[] array){
+        int result = 0;
+        //Hier muss Quellcode ergänzt werden.
+        return result;
+    }
+
+
+    /**
+     * 3. Negative Werte aufsummieren
+     * Schreiben Sie eine Methode sumUpNegatives, die als Parameter ein Array des Typs int übergeben bekommt. Die Methode sumUpNegatives soll die Summe der negativen Werte in diesem Array zurückgeben.
+     * @param array
+     * @return
+     */
+    private static int sumUpNegatives(int[] array){
+        int result = 0;
+        //Hier muss Quellcode ergänzt werden.
+        return result;
+    }
+
+
+
+    /** 4. Bestimmen des Maximums in einem Feld
+     * Schreiben Sie eine Methode findMaximum, die ein Array des Typs int als Parameter
+     * übergeben bekommt und die den größten Wert in diesem Array zurückgibt.
+     * Tipp: Versuchen Sie, mit einem Durchlauf durch das Array zu kommen.
+     */
+
+
+
+    /** 5. Bestimmen des Index des Maximums in einem Feld
+     * Schreiben Sie eine Methode findMaximumIndex, die ein Array des Typs int als Parameter
+     * übergeben bekommt und die den Index des größten Werts in diesem Array zurückgibt.
+     * Tipp: Versuchen Sie, mit einem Durchlauf durch das Array zu kommen.
+     */
+
+
+
+    /** 6. Bestimmen der Häufigkeit des Maximums in einem Feld
+     * Schreiben Sie eine Methode countMaximum, die ein Array des Typs int als Parameter
+     * übergeben bekommt und die Häufigkeit der größten Zahl in diesem Array zurückgibt.
+     */
+
+
+
+    /** 7. Sortierung prüfen
+     * Schreiben Sie eine Methode isSorted, die als Parameter ein Array des Typs int
+     * übergeben bekommt. Die Methode isSorted soll true zurückgeben, falls die im Array enthaltenen Werte aufsteigend sortiert sind.
+     * Sonst soll false zurückgegeben werden.
+     */
+
+
+
+    /** 8. Palindrome
+     * Ein Palindrom ist eine Folge von int-Werten, die vorwärts und rückwärts identisch gelesen werden kann.
+     * Beispiele: 12 34 78 34 12 oder 5 17 85 85 17 5
+     * Schreiben Sie eine Methode checkArray, die ein Array des Typs int übergeben bekommt und überprüft,
+     * ob es sich bei dem Array um ein Palindrom handelt.
+     * Die Methode soll einen Wert des Typs boolean zurückgeben.
+     */
+
+
+
+    /** 9. Erhöhen der Inhalte eines Feldes
+     * Schreiben Sie eine Methode increaseArray, die als Parameter ein Array des Typs int und einen int-Wert erhält.
+     * Die Methode increaseArray soll alle Werte des Arrays um den im zweiten Parameter übergebenen Wert erhöhen und als neues Array zurückgeben.
+     * Beispiel: Werden ein Array mit den Elementen 80,7,1,56,11,72,43,37 als erstes und der Wert 17 als zweites Argument übergeben,
+     * so soll ein neues(!) Array mit den Werten 97,24,18,73,28,89,60,54 zurückgegeben werden.
+     */
+
+
+}
