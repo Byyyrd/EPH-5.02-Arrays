03e5242ef4b6055a8f0a3a073a81c15d1e808b81
Byyyrd <105438850+Byyyrd@users.noreply.github.com>
2023-05-08 20:56:40 +0200

    Alle Aufgaben

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -7,12 +7,13 @@ class MainProgram {
     //Deklaration
     private static int[] numbers;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         withNegatives = true;
         maxAbsolute = 100;
         numbers = new int[10];
         fillArray(numbers, withNegatives, maxAbsolute);
-
+        numbers = new int[]{0, 1, 2, 4, 2, 1, 0};
+        numbers = increaseArray(numbers,1);
         showArray(numbers);
 
         //Hier sehen Sie Beispiele, wie man Methoden mit Rückgabewert aufrufen und dank der Systemkonsole testen kann.
@@ -21,29 +22,32 @@ class MainProgram {
         System.out.println("In diesem Array gibt es " + countNegatives(numbers) + " negative Zahlen.");
         System.out.println("Summe der negativen Zahlen: " + sumUpNegatives(numbers));
         System.out.println("Höhste Zahl: " + findMaximum(numbers) + " mit dem Index: " + findMaximumIndex(numbers));
+        System.out.println("Sortiert?: " + isSorted(numbers));
+        System.out.println("Palindrom?: " + checkArray(numbers));
     }
 
-    private static void fillArray(int[] array, boolean wN, int maxA){
-        if(wN){
-            for(int i = 0; i < array.length; i++){
-                int power = (int)(Math.random()*2);
-                array[i] = (int)(Math.pow(-1,power)*(Math.random()*(maxA+1)));
+    private static void fillArray(int[] array, boolean wN, int maxA) {
+        if (wN) {
+            for (int i = 0; i < array.length; i++) {
+                int power = (int) (Math.random() * 2);
+                array[i] = (int) (Math.pow(-1, power) * (Math.random() * (maxA + 1)));
             }
-        }else{
-            for(int i = 0; i < numbers.length; i++){
-                array[i] = (int)(Math.random()*(maxA+1));
+        } else {
+            for (int i = 0; i < numbers.length; i++) {
+                array[i] = (int) (Math.random() * (maxA + 1));
             }
         }
     }
 
     /**
      * Diese Methode stellt alle Werte des übergeben Arrays samt dazugehörigem Index in der Systemkonsole dar.
+     *
      * @param array
      */
-    private static void showArray(int[] array){
-        if(array != null){
-            for(int i = 0; i < array.length; i++){
-                System.out.println("Index: "+i+" --- Zahl: "+array[i]);
+    private static void showArray(int[] array) {
+        if (array != null) {
+            for (int i = 0; i < array.length; i++) {
+                System.out.println("Index: " + i + " --- Zahl: " + array[i]);
             }
         }
     }
@@ -52,10 +56,11 @@ class MainProgram {
     /**
      * 1. Alle Werte aufaddieren
      * Schreiben Sie eine Methode sumUp, die als Parameter ein Array des Typs int übergeben bekommt. Die Methode sumUp soll die enthaltenen Zahlen addieren und das Ergebnis zurückliefern.
+     *
      * @param array
      * @return Summe aller Zahlen im Array
      */
-    private static int sumUp(int[] array){
+    private static int sumUp(int[] array) {
         int result = 0;
         //Hier muss Quellcode ergänzt werden.
         for (int i = 0; i < array.length; i++) {
@@ -68,13 +73,14 @@ class MainProgram {
     /**
      * 2. Negative Werte zählen - hierzu bitte in Zeile 11 withNegatives = true setzen.
      * Schreiben Sie eine Methode countNegatives, die als Parameter ein Array des Typs int übergeben bekommt. Die Methode countNegatives soll die negativen Werte in diesem Array zählen und das Ergebnis als zurückgeben.
+     *
      * @param array
      * @return Anzahl der negativen Zahlen im Array
      */
-    private static int countNegatives(int[] array){
+    private static int countNegatives(int[] array) {
         int result = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] < 0){
+            if (array[i] < 0) {
                 result++;
             }
         }
@@ -85,10 +91,11 @@ class MainProgram {
     /**
      * 3. Negative Werte aufsummieren
      * Schreiben Sie eine Methode sumUpNegatives, die als Parameter ein Array des Typs int übergeben bekommt. Die Methode sumUpNegatives soll die Summe der negativen Werte in diesem Array zurückgeben.
+     *
      * @param array
      * @return Summe aller negativen Zahlen im Array
      */
-    private static int sumUpNegatives(int[] array){
+    private static int sumUpNegatives(int[] array) {
         int result = 0;
         //Hier muss Quellcode ergänzt werden.
         for (int i = 0; i < array.length; i++) {
@@ -100,16 +107,16 @@ class MainProgram {
     }
 
 
-
-    /** 4. Bestimmen des Maximums in einem Feld
+    /**
+     * 4. Bestimmen des Maximums in einem Feld
      * Schreiben Sie eine Methode findMaximum, die ein Array des Typs int als Parameter
      * übergeben bekommt und die den größten Wert in diesem Array zurückgibt.
      * Tipp: Versuchen Sie, mit einem Durchlauf durch das Array zu kommen.
      */
-    private static int findMaximum(int[] array){
+    private static int findMaximum(int[] array) {
         int max = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] > max){
+            if (array[i] > max) {
                 max = array[i];
             }
         }
@@ -117,17 +124,17 @@ class MainProgram {
     }
 
 
-
-    /** 5. Bestimmen des Index des Maximums in einem Feld
+    /**
+     * 5. Bestimmen des Index des Maximums in einem Feld
      * Schreiben Sie eine Methode findMaximumIndex, die ein Array des Typs int als Parameter
      * übergeben bekommt und die den Index des größten Werts in diesem Array zurückgibt.
      * Tipp: Versuchen Sie, mit einem Durchlauf durch das Array zu kommen.
      */
-    private static int findMaximumIndex(int[] array){
+    private static int findMaximumIndex(int[] array) {
         int max = 0;
         int index = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] > max){
+            if (array[i] > max) {
                 max = array[i];
                 index = i;
             }
@@ -136,16 +143,16 @@ class MainProgram {
     }
 
 
-
-    /** 6. Bestimmen der Häufigkeit des Maximums in einem Feld
+    /**
+     * 6. Bestimmen der Häufigkeit des Maximums in einem Feld
      * Schreiben Sie eine Methode countMaximum, die ein Array des Typs int als Parameter
      * übergeben bekommt und die Häufigkeit der größten Zahl in diesem Array zurückgibt.
      */
-    private static int countMaximum(int[] array){
+    private static int countMaximum(int[] array) {
         int max = findMaximum(array);
         int count = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == max){
+            if (array[i] == max) {
                 count++;
             }
         }
@@ -153,15 +160,15 @@ class MainProgram {
     }
 
 
-
-    /** 7. Sortierung prüfen
+    /**
+     * 7. Sortierung prüfen
      * Schreiben Sie eine Methode isSorted, die als Parameter ein Array des Typs int
      * übergeben bekommt. Die Methode isSorted soll true zurückgeben, falls die im Array enthaltenen Werte aufsteigend sortiert sind.
      * Sonst soll false zurückgegeben werden.
      */
-    private static boolean isSorted(int[] array){
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > array[i+1]){
+    private static boolean isSorted(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
                 return false;
             }
         }
@@ -169,15 +176,22 @@ class MainProgram {
     }
 
 
-
-    /** 8. Palindrome
+    /**
+     * 8. Palindrome
      * Ein Palindrom ist eine Folge von int-Werten, die vorwärts und rückwärts identisch gelesen werden kann.
      * Beispiele: 12 34 78 34 12 oder 5 17 85 85 17 5
      * Schreiben Sie eine Methode checkArray, die ein Array des Typs int übergeben bekommt und überprüft,
      * ob es sich bei dem Array um ein Palindrom handelt.
      * Die Methode soll einen Wert des Typs boolean zurückgeben.
      */
-
+    private static boolean checkArray(int[] array) {
+        for (int i = 0; i < array.length /2; i++) {
+            if (array[i] != array[array.length - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 
     /** 9. Erhöhen der Inhalte eines Feldes
@@ -187,5 +201,11 @@ class MainProgram {
      * so soll ein neues(!) Array mit den Werten 97,24,18,73,28,89,60,54 zurückgegeben werden.
      */
 
-
+    private static int[] increaseArray(int[] array,int amount) {
+        int[] result = new int[array.length];
+        for (int i = 0; i < array.length ; i++) {
+            result[i] = array[i] + amount;
+        }
+        return result;
+    }
 }
